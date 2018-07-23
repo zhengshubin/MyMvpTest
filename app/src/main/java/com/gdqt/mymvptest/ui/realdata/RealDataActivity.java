d7c7707ccc0788129c3cd7bf21390b98f3032831
zhengshubin <804718581@qq.com>
2018-07-23 19:02:07 +0800

    Main 初始化布局

--- app/src/main/java/com/gdqt/mymvptest/ui/realdata/RealDataActivity.java
+++ app/src/main/java/com/gdqt/mymvptest/ui/realdata/RealDataActivity.java
@@ -1,6 +1,7 @@
 package com.gdqt.mymvptest.ui.realdata;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 
 import com.gdqt.mymvptest.R;
@@ -15,24 +16,13 @@ public class RealDataActivity extends BaseActivity implements IRealDataView {
     @BindView(R.id.rv_realData)
     RecyclerView rvRealData;
     @BindView(R.id.refresh_realData)
-    PullToRefreshView refreshRealData;
+    SwipeRefreshLayout refreshRealData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real_data);
-      refreshRealData.setOnRefreshListener(new PullToRefreshView.OnRefreshListener() {
-          @Override
-          public void onRefresh() {
-              refreshRealData.postDelayed(new Runnable() {
-                  @Override
-                  public void run() {
-                      refreshRealData.setRefreshing(false);
 
-                  }
-              },4000);
-          }
-      });
 
 
     }
