23d66f8e0e5411ac54621e1b439fba8e111f1375
zhengshubin <804718581@qq.com>
2018-07-23 09:06:45 +0800

    2018723

--- app/src/main/java/com/gdqt/mymvptest/Application/MyApplication.java
+++ app/src/main/java/com/gdqt/mymvptest/Application/MyApplication.java
@@ -14,6 +14,12 @@ public class MyApplication extends Application{
     public void onCreate() {
         super.onCreate();
         mContext = getApplicationContext();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
     }
     public static  Context getContext(){
         return mContext;
